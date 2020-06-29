effc17d0b0a1694f2387ff2171ea05ec441f6b50
CcQun <13698603020@163.com>
2020-06-29 17:07:26 +0800

    添加Controller和Utils

---
+++ src/main/java/com/example/demo/controller/ApiController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author CcQun
+ * @Date 2020/6/29 17:05
+ */
+@RestController()
+@RequestMapping("/api")
+public class ApiController {
+}
