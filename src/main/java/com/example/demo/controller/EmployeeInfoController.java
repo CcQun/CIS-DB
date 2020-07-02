5b5f3041dd34f8d929aaaed03dc9947945807337
OKlala1999 <17301092@bjtu.edu.cn>
2020-07-02 14:51:53 +0800

    其他信息统计

--- src/main/java/com/example/demo/controller/EmployeeInfoController.java
+++ src/main/java/com/example/demo/controller/EmployeeInfoController.java
@@ -1,10 +1,15 @@
 package com.example.demo.controller;
 
+import com.example.demo.core.Utils;
 import com.example.demo.core.response.BaseResponse;
+import com.example.demo.core.response.StatResponse;
+import com.example.demo.db.model.EmployeeInfo;
 import com.example.demo.db.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @Author CcQun
@@ -35,5 +40,53 @@ public class EmployeeInfoController {
         this.oldpersonInfoService = oldpersonInfoService;
         this.sysUserService = sysUserService;
         this.volunteerInfoService = volunteerInfoService;
+    }
+
+    //员工信息统计
+    @RequestMapping("/statEmployee")
+    public StatResponse statEmployee() throws Exception {
+        StatResponse response = new StatResponse();
+        List<EmployeeInfo> list =  employeeInfoService.findAll();
+        int female=0;
+        int male=0;
+        int level1=0;
+        int level2=0;
+        int level3=0;
+        int total=list.size();
+        if(total<=0){
+            response.setMsg("数据库中无工作人员信息");
+            response.setCode(0);
+            return response;
+        }else{
+            for(int i=0;i<total;i++){
+                EmployeeInfo employeeInfo = list.get(i);
+                int age = Utils.getAge(employeeInfo.getBirthday());
+                System.out.println(employeeInfo.getUsername()+"_age:"+age);
+                //统计年龄分布
+                if(age<30){
+                    level1++;
+                }else if(age>=30&&age<40){
+                    level2++;
+                }else{
+                    level3++;
+                }
+
+                //统计性别
+                if(employeeInfo.getGender().equals("男")){
+                    male++;
+                }else {
+                    female++;
+                }
+            }
+        }
+        response.setTotalNumber(total);
+        response.setNumberOfFe(female);
+        response.setNumberOfMa(male);
+        response.setNumberOfL1(level1);
+        response.setNumberOfL2(level2);
+        response.setNumberOfL3(level3);
+        response.setCode(1);
+        response.setMsg("统计信息返回");
+        return response;
     }
 }
--- src/main/java/com/example/demo/controller/VolunteerInfoController.java
+++ src/main/java/com/example/demo/controller/VolunteerInfoController.java
@@ -1,9 +1,14 @@
 package com.example.demo.controller;
 
+import com.example.demo.core.Utils;
+import com.example.demo.core.response.StatResponse;
+import com.example.demo.db.model.VolunteerInfo;
 import com.example.demo.db.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @Author CcQun
@@ -33,5 +38,53 @@ public class VolunteerInfoController {
         this.oldpersonInfoService = oldpersonInfoService;
         this.sysUserService = sysUserService;
         this.volunteerInfoService = volunteerInfoService;
+    }
+
+    @RequestMapping("/statVolunteer")
+    public StatResponse statVolunteer() throws Exception {
+        StatResponse response = new StatResponse();
+        List<VolunteerInfo> list = volunteerInfoService.findAll();
+        int female=0;
+        int male=0;
+        int level1=0;
+        int level2=0;
+        int level3=0;
+        int total=list.size();
+        if(total<=0){
+            response.setMsg("数据库中无义工信息");
+            response.setCode(0);
+            return response;
+        }else{
+            for(int i=0;i<total;i++){
+                VolunteerInfo volunteerInfo = list.get(i);
+                int age= Utils.getAge(volunteerInfo.getBirthday());
+                System.out.println(volunteerInfo.getName()+"_age:"+age);
+
+                //年龄统计
+                if(age<40){
+                    level1++;
+                }else if(age>=40&&age<50){
+                    level2++;
+                }else {
+                    level3++;
+                }
+                //统计性别
+                if (volunteerInfo.getGender().equals("男")){
+                    male++;
+                }else {
+                    female++;
+                }
+            }
+        }
+
+        response.setTotalNumber(total);
+        response.setNumberOfFe(female);
+        response.setNumberOfMa(male);
+        response.setNumberOfL1(level1);
+        response.setNumberOfL2(level2);
+        response.setNumberOfL3(level3);
+        response.setCode(1);
+        response.setMsg("统计信息返回");
+        return response;
     }
 }
