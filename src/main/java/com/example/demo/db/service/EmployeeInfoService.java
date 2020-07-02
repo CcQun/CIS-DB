e2c2c6580371dfc1c25156791e30f7c559bb2602
zsmm <17301086@bjtu.edu.cn>
2020-07-02 11:19:17 +0800

    老人信息管理修改以及员工信息管理全部

--- src/main/java/com/example/demo/db/service/EmployeeInfoService.java
+++ src/main/java/com/example/demo/db/service/EmployeeInfoService.java
@@ -2,12 +2,42 @@ package com.example.demo.db.service;
 
 import com.example.demo.db.mapper.EmployeeInfoMapper;
 import com.example.demo.db.model.EmployeeInfo;
+import com.example.demo.db.model.OldpersonInfo;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 /**
- * @Author CcQun
- * @Date 2020/6/29 21:06
+ * @Author CcQun zsm
+ * @Date begin:2020/07/01  end:
  */
 @Service
 public class EmployeeInfoService extends BaseService<EmployeeInfo,Integer, EmployeeInfoMapper>{
+
+    public EmployeeInfo findEmployeeByID(Integer id)
+    {
+        System.out.println("id:"+id);
+        EmployeeInfo employee= EmployeeInfo.builder()
+                .id(id).build();
+        Example<EmployeeInfo> example = Example.of(employee);
+        List<EmployeeInfo> list = mapper.findAll(example);
+        System.out.println(list.size());
+        return list.get(0);
+    }
+
+    public EmployeeInfo findEmployeeById_card(String id_card)
+    {
+        EmployeeInfo employee= EmployeeInfo.builder()
+                .id_card(id_card).build();
+
+        Example<EmployeeInfo> example = Example.of(employee);
+        List<EmployeeInfo> list = mapper.findAll(example);
+        if(list.size()>0){
+            return list.get(list.size()-1);
+        }else{
+            return null;
+        }
+
+    }
 }
